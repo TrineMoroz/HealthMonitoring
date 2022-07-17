f176292785152e83be3299fa0f7e1c52c5527da4
Ekaterina <bateeva-k@mail.ru>
2022-07-17 10:20:58 +0300

    Initial Commit

---
+++ HealthMonitoring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthMonitoring
+//
+//  Created by Екатерина Батеева on 17.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
